8fc25dc766d2d800ae22a8b9f19a7f20080b2b68
mianrafaqat <rafaqatmehar007@gmail.com>
2023-09-20 11:35:25 +0500

    split the components

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,14 @@
 import React, { useState } from "react";
+import Search from "./Search";
+import Logo from "./Logo";
+import Numresults from "./Numresults";
 
 const Navbar = () => {
-  const [query, setQuery] = useState("");
   return (
     <nav className="nav-bar">
-      <div className="logo">
-        <span role="img">🍿</span>
-        <h1>usePopcorn</h1>
-      </div>
-      <input
-        className="search"
-        type="text"
-        placeholder="Search movies..."
-        value={query}
-        onChange={(e) => setQuery(e.target.value)}
-      />
-      <p className="num-results">
-        Found <strong>X</strong> results
-      </p>
+      <Logo />
+      <Search />
+      <Numresults />
     </nav>
   );
 };
---
+++ src/components/Numresults.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Numresults = () => {
+  return (
+    <p className="num-results">
+      Found <strong>X</strong> results
+    </p>
+  );
+};
+
+export default Numresults;
